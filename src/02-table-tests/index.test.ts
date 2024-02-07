008f871b9377283b39ec397bec82f46598548b3c
“Stanislav <gorokht6mail.ru@gmail.com>
2024-02-07 21:51:35 +0200

    feat: create test cases for unit test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,12 +1,16 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import {  simpleCalculator, Action } from './index';
 
 const testCases = [
     { a: 1, b: 2, action: Action.Add, expected: 3 },
     { a: 2, b: 2, action: Action.Add, expected: 4 },
     { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { a: 10, b: 4, action: Action.Subtract, expected: 6 },
+    { a: 5, b: 4, action: Action.Multiply, expected: 20 },
+    { a: 25, b: 5, action: Action.Divide, expected: 5 },
+    { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+    { a: "a", b: 3, action: Action.Exponentiate, expected: null },
+    { a: 2, b: 3, action: "divide", expected: null },
+];
 
 describe('simpleCalculator', () => {
   // This test case is just to run this test suite, remove it when you write your own tests
