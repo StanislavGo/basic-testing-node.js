19721aa79f281b41479f51916de4b573f7a9f3b3
“Stanislav <gorokht6mail.ru@gmail.com>
2024-02-07 22:52:48 +0200

    feat: create unit test to test if an error is thrown

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,19 +1,19 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import { throwError, /* throwCustomError, */ resolveValue, /* MyAwesomeError, rejectCustomError */ } from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const data = await resolveValue(10);
+    expect(data).toBe(10);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect(() => throwError("Works")).toThrow("Works");
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => throwError(undefined)).toThrow("Oops!");
   });
 });
 
